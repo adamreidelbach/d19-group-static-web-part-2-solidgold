0c4f2e360be97ca7a6dd333ad49b3f3516c35918
Ryan McCarty <Ryan McCarty>
2017-04-14 13:14:43 -0500

    contact page, sytle, js, and images

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var products= [{name:"lamp1",description:" this is the first lamp ",price:" $10.00", url: "images/lamp1.jpeg"},
-			{name:"lamp2",description:" this is the second lamp ",price:" $10.00", url: "images/lamp2.jpeg"},
-			{name:"lamp3",description:" this is the third lamp ",price:" $10.00", url: "images/lam3.jpeg"},
-			{name:"lamp4",description:" this is the fourth lamp ",price:" $10.00", url: "images/lamp4.jpeg"},
-			{name:"lamp5",description:" this is the fith lamp ",price:" $10.00", url: "images/lamp5.jpeg"},
-			{name:"lamp6",description:" this is the sixth lamp ",price:" $10.00", url: "images/lamp6.jpeg"},
-			{name:"lamp7",description:" this is the seventh lamp ",price:" $10.00", url: "images/lamp7.jpeg"},
-			{name:"lamp8",description:" this is the eigth lamp ",price:" $10.00", url: "images/lamp8.jpeg"},
-			{name:"lamp9",description:" this is the ninth lamp ",price:" $10.00", url: "images/lamp9.jpeg"}
+var products= [{name:"Glass bodied white lamp",description:" All white lamp with a glass body ",price:" $55.00", url: "images/lamp1.jpeg"},
+			{name:"Desk lamp ",description:" Angled neck with platform ",price:" $29.99", url: "images/lamp2.jpeg"},
+			{name:"Table lamp ",description:" All black with a touch activatioin",price:" $18.00", url: "images/lam3.jpeg"},
+			{name:"Miniature street lamp",description:" Styled after common streep lamps with a metal frame ",price:" $40.00", url: "images/lamp4.jpeg"},
+			{name:"Brass bodied lamp",description:" Polished brass body that comes with an assorment of shades ",price:" $35.00", url: "images/lamp5.jpeg"},
+			{name:"Table lamp",description:" Angled neck, perfect for a reading light",price:" $25.00", url: "images/lamp6.jpeg"},
+			{name:"Open table lamp",description:" Stylish home decor ",price:" $120.00", url: "images/lamp7.jpeg"},
+			{name:"Ceramic boddied lamp",description:" Made from ceramic on the body, and comes with a white parchment shade ",price:" $100.00", url: "images/lamp8.jpeg"},
+			{name:"Floor Lamp",description:" Great for sitting in the corner of your home",price:" $89.99", url: "images/lamp9.jpeg"}
 ];
 
 var divElement= document.getElementById('test');
